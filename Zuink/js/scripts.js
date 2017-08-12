9a8030089ac5e065147a1f666b76f339140f950f
panarindaniil <panarin.daniil@gmail.com>
2017-08-12 17:29:25 +0300

    form

---
+++ Zuink/js/scripts.js
@@ -0,0 +1,12 @@
+ $(document).ready(function(){
+ 	$('.offers').slick({
+	  infinite: true,
+	  slidesToShow: 4,
+	  slidesToScroll: 1
+	});
+	$('.portfolio-list').slick({
+	  infinite: true,
+	  slidesToShow: 4,
+	  slidesToScroll: 1
+	});
+});
