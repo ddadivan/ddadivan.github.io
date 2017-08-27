99c8b986769b20fbb73611f06f0d972f7b1de211
PanarinDaniil <panarin.daniil@gmail.com>
2017-08-27 19:21:22 +0300

    fly studio init

---
+++ fly-studio/js/scripts.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    if ( $(".tabs").width() > 0 ) {
+        $( function() {
+            $( ".tabs" ).tabs();
+        });
+    }
+    if ( $(".port-list").width() > 0 ) {
+        $('.port-list').slick({
+          dots: false,
+          arrows: true,
+          infinite: false,
+          speed: 300,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          swipeToSlide: true,
+          responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        });
+    }
+    if ( $(".logo-list").width() > 0 ) {
+        $('.logo-list').slick({
+          dots: false,
+          arrows: true,
+          infinite: false,
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          swipeToSlide: true
+        });
+    }
+    if ( $(".testimonials-list").width() > 0 ) {
+        $('.testimonials-list').slick({
+          dots: false,
+          arrows: true,
+          infinite: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          swipeToSlide: true
+        });
+    }
+});
