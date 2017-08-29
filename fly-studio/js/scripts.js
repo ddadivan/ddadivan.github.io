8772f1df53842c2771c55a284a440a6107556937
Dan Panarin <panarindaniil@gmail.com>
2017-08-29 11:33:57 +0300

    slider on tabs callback portfolio

--- fly-studio/js/scripts.js
+++ fly-studio/js/scripts.js
@@ -1,8 +1,29 @@
 $(document).ready(function(){
     if ( $(".tabs").width() > 0 ) {
-        $( function() {
-            $( ".tabs" ).tabs();
-        });
+      $( function() {
+          $( ".tabs" ).tabs();
+      });
+      $( ".tabs" ).on( "tabsactivate", function( event, ui ) {
+          $('.port-list').slick('unslick');
+          $('.port-list').slick({
+            dots: false,
+            arrows: true,
+            infinite: false,
+            speed: 300,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            swipeToSlide: true,
+            responsive: [
+              {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+            ]
+          });
+      });
     }
     if ( $(".port-list").width() > 0 ) {
         $('.port-list').slick({
