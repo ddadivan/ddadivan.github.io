ff9e48c9de745ea28c8c29b9dd9325a1bae08ccb
ddadivan <dmitriydadivan@gmail.com>
2017-10-03 04:01:59 +0300

    buket

---
+++ byketik/js/scripts.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+  $('.gallery').slick({
+        centerMode: true,
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 900,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        swipeToSlide: true,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+  });
+
+$(document).ready(function(){
+  $('.insta').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 900,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        swipeToSlide: true,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+  });
+
+$(document).ready(function(){
+  $('.work').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        speed: 900,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        swipeToSlide: true,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+  });
