2f64046b9c357f00c99cc9821e2185d0a86bde93
ddadivan <dmitriydadivan@gmail.com>
2017-10-04 13:09:57 +0300

    all

--- byketik/js/scripts.js
+++ byketik/js/scripts.js
@@ -62,3 +62,12 @@ $(document).ready(function(){
         ]
       });
   });
+
+$(document).ready(function(){
+    $(".down").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top-100}, 1500);
+    });
+});
