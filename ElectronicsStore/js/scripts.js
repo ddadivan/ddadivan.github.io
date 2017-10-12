07813eae09a8eff69b6fa02ee2c1fe90e35ad7a5
Dan Panarin <panarindaniil@gmail.com>
2017-10-12 15:11:17 +0300

    ашчуы ащк ефииув ыдшвук

--- ElectronicsStore/js/scripts.js
+++ ElectronicsStore/js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-      $(".tabbed-slider a").on("click", function(){
+    $(".tabbed-slider a").on("click", function(){
         $(".tabbed-slider a").removeClass("active");  
         $(this).addClass("active");  
         var imgNumber = $(this).attr('data-number');  
         $(".tabbedImg").removeClass("active");  
-        $(".tabbedImg-" + imgNumber).addClass("active"); 
-      });
+        $(".imgNumber-" + imgNumber).addClass("active"); 
+    });
 });
