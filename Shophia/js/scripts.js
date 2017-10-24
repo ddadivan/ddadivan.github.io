62a83e75f6f61dc07b00d0c2bb57c9916fcc124e
ddadivan <dmitriydadivan@gmail.com>
2017-10-24 16:55:49 +0300

    new

---
+++ Shophia/js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        swipeToSlide: true,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+  });
