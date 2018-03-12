b9679163137f57165735c60ab46899255412a161
ddadivan <dmitriydadivan@gmail.com>
2018-03-12 13:49:06 +0200

    Slick And Classy

--- slick/src/js/scripts.js
+++ slick/src/js/scripts.js
@@ -1,20 +1,32 @@
 $(document).ready(function(){
   $('.slider-box').slick({
-      dots: false,
-      arrows:true,
-      infinite: true,
-      speed: 700,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      swipeToSlide: true,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
+    dots: false,
+    arrows:true,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
-      ]
-    });
+      }
+    ]
+  });
+  $('.mob-btn').click(function() {
+    $('.nav').toggleClass('active');
+    $(this).toggleClass('active');
+    $('.layout').toggleClass('active');
+    $('body').toggleClass('no-scroll');
+  });
+  $('.layout').click(function() {
+    $('.nav').removeClass('active');
+    $(this).removeClass('active');
+    $('.mob-btn').removeClass('active');
+    $('body').removeClass('no-scroll');
+  });
 });
